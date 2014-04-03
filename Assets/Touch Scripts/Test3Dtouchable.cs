9237cf034e0ab6be358a180df77bc13abac68e38
Niamh's laptop <gallagherj7@gmail.com>
2014-04-03 04:27:15 +0100

    test touch cosmo moving
    touch doc from script worked, reexamine and implement

--- Assets/Touch Scripts/Test3Dtouchable.cs
+++ Assets/Touch Scripts/Test3Dtouchable.cs
@@ -28,17 +28,24 @@ public class Test3Dtouchable : MonoBehaviour,ITouchable3D {
 	}
 	public void OnTouchMoved ()
 	{
+
+			if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+						Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
+						transform.Translate (-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+				}
 		//z of ScreenToWorldPoint is distance from camera into the world, so we need to find this object's distance from the camera to make it stay on the same plane
-		Vector3 tempTouch = new Vector3(Input.GetTouch(touch2Watch).position.x, Input.GetTouch(touch2Watch).position.y,camTrans.position.y - myTrans.position.y);
+//		Vector3 tempTouch = new Vector3(Input.GetTouch(touch2Watch).position.x, Input.GetTouch(touch2Watch).position.y,camTrans.position.y - myTrans.position.y);
 		//Convert screen to world space
-		finger = Camera.main.ScreenToWorldPoint(tempTouch);
-		
+	//	finger = Camera.main.ScreenToWorldPoint(tempTouch);
+
 		//look at finger position
-		myTrans.LookAt(finger);
+//		myTrans.LookAt(finger);
 		
 		//move towards finger if not too close
-		if(Vector3.Distance(finger, myTrans.position) > maxDist)
-			myTrans.Translate (Vector3.forward * speed * Time.deltaTime);
+//		if(Vector3.Distance(finger, myTrans.position) > maxDist)
+//			myTrans.Translate (Vector3.forward * speed * Time.deltaTime);
+
+		Debug.Log ("moved,yo");
 	}
 	public void OnTouchStayed ()
 	{
